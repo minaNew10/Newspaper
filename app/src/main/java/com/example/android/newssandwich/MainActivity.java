9e082ae35e96ba2e0944704f90e6d7224ae9f9d4
mina isaac <mina2011new@gmail.com>
2018-07-14 07:12:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/newssandwich/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.newssandwich;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //api key = b70d4f5d-e00b-44c3-8d15-6c700f643073
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
